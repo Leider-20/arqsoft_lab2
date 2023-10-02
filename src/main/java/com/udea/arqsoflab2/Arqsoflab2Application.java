d00de80b260ddb62804aacf668aaf49b0cf5b67d
LeiderF <leider.caicedo@udea.edu.co>
2023-10-01 19:50:33 -0500

    Primer commit

---
+++ src/main/java/com/udea/arqsoflab2/Arqsoflab2Application.java
@@ -0,0 +1,13 @@
+package com.udea.arqsoflab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arqsoflab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Arqsoflab2Application.class, args);
+	}
+
+}
